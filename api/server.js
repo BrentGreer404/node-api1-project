fb6887d4d71577247c865513362a446681549ac9
BrentGreer404 <yem1546@yahoo.com>
2023-02-05 15:09:48 -0500

    added update user

--- api/server.js
+++ api/server.js
@@ -39,6 +39,31 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({ 
+        message: "The user with the specified ID does not exist" 
+      })
+    } else {
+      if (!req.body.name || !req.body.bio){
+        res.status(400).json({
+          message: "Please provide name and bio for the user"
+        })
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body)
+        res.status(200).json(updatedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error getting user",
+      err: err.message
+    })
+  }
+})
+
 server.post('/api/users', (req, res) => {
   const user = req.body;
   if (!user.name || !user.bio){
