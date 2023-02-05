bf6d5f3bddf6b616698e09c3b80e1c0c4ea671da
BrentGreer404 <yem1546@yahoo.com>
2023-02-05 14:13:57 -0500

    added get by id

--- api/server.js
+++ api/server.js
@@ -1,16 +1,48 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const user = require('./users/model')
 const server = express()
 
+
+
 server.get('/', (req, res) => {
-  res.send("Welcome")
+  res.json("Welcome")
 })
 
 server.get('/api/users', (req, res) => {
-  res.send("users")
-} )
+  user.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message
+      })
+    })
+})
 
+server.get('/api/users/:id', (req, res) => {
+  user.findById(req.params.id)
+    .then(user => {
+      res.json(user)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "error getting user",
+        err: err.message
+      })
+    })
+})
 
+// server.post('/api/users', (req, res) => {
+//   req.insert({user.name, user.bio})
+// })
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: "not found"
+  })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
